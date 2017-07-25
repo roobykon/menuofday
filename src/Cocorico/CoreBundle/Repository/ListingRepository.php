944d70687c95f8da238f7611530d9b52cb527cc7
tema_on <tema_on@ukr.net>
2017-07-25 17:19:17 +0300

    fix cache time

--- src/Cocorico/CoreBundle/Repository/ListingRepository.php
+++ src/Cocorico/CoreBundle/Repository/ListingRepository.php
@@ -258,7 +258,7 @@ class ListingRepository extends EntityRepository
             ->orderBy('l.createdAt', 'DESC');
         try {
             $query = $queryBuilder->getQuery();
-            $query->useResultCache(true, 21600, 'findByHighestRanking');
+            $query->useResultCache(true, 60, 'findByHighestRanking');
 
             return $query->getResult();
         } catch (NoResultException $e) {
